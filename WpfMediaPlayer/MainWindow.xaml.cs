854e146a0014fbff4c08156ed21583d3ee63d976
Quang Trần <buuquang102@gmail.com>
2023-12-30 22:23:50 +0700

    fix bug

--- WpfMediaPlayer/MainWindow.xaml.cs
+++ WpfMediaPlayer/MainWindow.xaml.cs
@@ -503,6 +503,10 @@ namespace WpfMediaPlayer
                     // Thực hiện các bước để khôi phục thông tin vào ứng dụng
                     LoadPlaylistButtonClick(savedPlaylist);
                     currentPlaylistIndex = savedVideoIntValue;
+                    if (currentPlaylistIndex >= playlistListBox.Items.Count)
+                    {
+                        currentPlaylistIndex = 0;
+                    }
                     PlayMedia();
                     progressSlider.Value = savedPositionDoubleValue;
                     mediaPlayer.Position = TimeSpan.FromSeconds(savedPositionDoubleValue);
@@ -514,12 +518,7 @@ namespace WpfMediaPlayer
             {
                 //System.Windows.MessageBox.Show("a");
                 currentPlaylistIndex = 0;
-                //var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-                //config.AppSettings.Settings["savedPlaylist"].Value = "";
-                //config.AppSettings.Settings["savedVideo"].Value = "";
-                //config.AppSettings.Settings["savedPosition"].Value = "";
-                //config.Save(ConfigurationSaveMode.Minimal);
-                //ConfigurationManager.RefreshSection("appSettings");
+                playlistListBox.Items.Clear();
             }
         }
     }
